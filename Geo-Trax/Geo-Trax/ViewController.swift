182943fdbea1a54ed182fb047876bf02123d09be
Steve Wall <syntakks@gmail.com>
2020-04-01 18:09:04 -0500

    Initial Commit

---
+++ Geo-Trax/Geo-Trax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geo-Trax
+//
+//  Created by Stephen Wall on 4/1/20.
+//  Copyright © 2020 syntaks.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
